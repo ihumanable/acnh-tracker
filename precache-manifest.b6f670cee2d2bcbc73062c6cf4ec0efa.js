36e89f8c4125b55fcba953ce774348c746c88793
Matt Nowack <mdnowack@gmail.com>
2020-07-04 09:38:14 -0700

    Updates

---
+++ precache-manifest.b6f670cee2d2bcbc73062c6cf4ec0efa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb7d3df8334a5fe89c5241d6f791e8da",
+    "url": "/acnh-tracker/index.html"
+  },
+  {
+    "revision": "9495b47aa45d19f81727",
+    "url": "/acnh-tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9ab5329ef421b036c39e",
+    "url": "/acnh-tracker/static/js/2.154ad894.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/acnh-tracker/static/js/2.154ad894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9495b47aa45d19f81727",
+    "url": "/acnh-tracker/static/js/main.be743f6b.chunk.js"
+  },
+  {
+    "revision": "0917d0db8a588e99c12c",
+    "url": "/acnh-tracker/static/js/runtime-main.82023d27.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/acnh-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
